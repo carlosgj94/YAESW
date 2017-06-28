c7efab3416853cb3f8f45548a36b78a4395c9027
Carlos Gonzalez <carlosgj94@gmail.com>
2017-06-28 18:22:48 +0200

    Added the add coin web

--- js/scripts.js
+++ js/scripts.js
@@ -19,8 +19,24 @@
 
         var ismobile=navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i);
         if(!ismobile){
-          document.getElementById('body').innerHTML = "Please use a phone. Cabron.";
+          //document.getElementById('body').innerHTML = "Please use a phone. Cabron.";
         }
+
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+function autoComplete(){
+$('input.autocomplete').autocomplete({
+  data: {
+    "Hectors": 'http://freedesignfile.com/upload/2015/07/Education-elements-with-H-vector-material.jpg',
+    "Fosters": 'https://s3-media1.fl.yelpcdn.com/bphoto/J63_IiG39I9Ru6TFUTLRIA/o.jpg',
+    "Batmans": 'https://s-media-cache-ak0.pinimg.com/736x/65/7d/b4/657db47bb9ba1193a8975ea3a0c67864--free-svg-files-for-silhouette-fonts-vinyl-svg-files-free.jpg'
+  },
+  limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+  onAutocomplete: function(val) {
+    // Callback function when value is autcompleted.
+  },
+  minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
+});
+}
